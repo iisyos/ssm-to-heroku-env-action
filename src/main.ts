90c022df1678b0d7e192824149b56dbf104a093b
iisyos <kosuke1216a@yahoo.co.jp>
2023-05-06 01:01:25 +0900

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,30 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {SSM} from 'aws-sdk'
+import {SSMParameterGetter} from './services/ssm-parameter-getter'
+import {HerokuParameterSetter} from './services/heroku-parameter-setter'
+import {ActionRunner} from './services/action-runner'
+const ssm = new SSM({region: 'ap-northeast-3'})
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+  const parameterGetter = new SSMParameterGetter(ssm)
+  const result = await parameterGetter.getParameters({
+    path: core.getInput('path'),
+    parameters: [],
+    nextToken: undefined
+  })
+  const herokuParameterSetter = new HerokuParameterSetter(
+    {
+      email: core.getInput('email'),
+      token: core.getInput('token'),
+      app: core.getInput('app')
+    },
+    result
+  )
+  herokuParameterSetter.setParameters()
 }
 
-run()
+const runner = new ActionRunner({
+  name: 'SSM ParameterStore to Heroku shipping',
+  cb: run
+})
+runner.run()
